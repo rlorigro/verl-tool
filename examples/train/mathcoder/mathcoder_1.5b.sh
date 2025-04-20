b4aefd75fcfaaa7f66e8ef1cf04af4348dcf4136
dongfujiang <dongfu.jdf@gmail.com>
2025-04-20 18:08:48 +0800

    remove wandb api key

--- examples/train/mathcoder/mathcoder_1.5b.sh
+++ examples/train/mathcoder/mathcoder_1.5b.sh
@@ -1,7 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-export WANDB_API_KEY="31800be459915fc29628b8c03920c3b526d64109"
-
-n_gpus_per_node=4
+n_gpus_per_node=8
 n_nodes=1
 rl_alg=grpo # gae(ppo) or grpo, if grpo, then better set n>1 otherwise the group norm can not be effective
 train_data=[data/mathcoder/code_train.parquet,\
