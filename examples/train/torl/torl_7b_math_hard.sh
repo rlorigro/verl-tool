dec95593b1092d8f3cc4b2e86e9b41fa81d288ff
Dongfu Jiang <82598202+jdf-prog@users.noreply.github.com>
2025-04-20 17:30:09 +0800

    Update torl_7b_math_hard.sh

--- examples/train/torl/torl_7b_math_hard.sh
+++ examples/train/torl/torl_7b_math_hard.sh
@@ -1,6 +1,3 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-export WANDB_API_KEY="31800be459915fc29628b8c03920c3b526d64109"
-
 n_gpus_per_node=8
 n_nodes=1
 rl_alg=grpo # gae(ppo) or grpo, if grpo, then better set n>1 otherwise the group norm can not be effective
