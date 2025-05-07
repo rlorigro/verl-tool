b4ccb67c77ec736aa2fb6aa378f9cd48284ba8af
dongfujiang <dongfu.jdf@gmail.com>
2025-05-08 03:16:35 +0800

    update

--- verl_tool/trainer/ppo/ray_trainer.py
+++ verl_tool/trainer/ppo/ray_trainer.py
@@ -189,8 +189,9 @@ class AgentRayPPOTrainer(RayPPOTrainer):
                         batch_keys=['input_ids', 'attention_mask', 'position_ids'],
                         non_tensor_batch_keys=['raw_prompt_ids']
                     )
-                for key in additional_non_tensor_keys:
-                    gen_batch.non_tensor_batch[key] = batch.non_tensor_batch[key]
+                if 'agent' in self.config.actor_rollout_ref.actor.strategy:
+                    for key in additional_non_tensor_keys:
+                        gen_batch.non_tensor_batch[key] = batch.non_tensor_batch[key]
 
                 is_last_step = self.global_steps >= self.total_training_steps
 
